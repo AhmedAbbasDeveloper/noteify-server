7b5d05e1f2334aca5e419f7a6b002415485a2026
Ahmed Abbas <theabbos@gmail.com>
2022-12-26 03:00:30 -0500

    Replicate 510d62cc6fcc92b551a9818af85ea98b7f721734 using nestjs

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
