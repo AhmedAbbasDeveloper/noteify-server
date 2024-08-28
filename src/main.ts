0bd5d836aa77d20cec3d6c33ff36b103304ae147
Ahmed Abbas <theabbos@gmail.com>
2024-08-28 19:14:22 -0400

    fix validation pipe

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT);
 }
 
